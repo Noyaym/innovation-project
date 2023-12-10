ba83e38eb3aadc4c98b70e9a4cdfa272d5a5e3a6
nanonoam <97607867+nanonoam@users.noreply.github.com>
2023-12-10 19:20:21 +0200

    Dear raspbery pi - delited un nesesery files

--- yolov8.py
+++ yolov8.py
@@ -1,80 +1,25 @@
 from ultralytics import YOLO
 import cv2
-import math 
-# import ntcore
 
-
-# start webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
-# inst = ntcore.NetworkTableInstance.getDefault()
-
-# inst.startClient4("example client")
-
-# # connect to a roboRIO with team number TEAM
-# inst.setServerTeam(5635)
-
-# # starting a DS client will try to get the roboRIO address from the DS application
-# inst.startDSClient()
-
-# inst.setServer("host", ntcore.NetworkTableInstance.kDefaultPort4)
-
-# # Get the table within that instance that contains the data. Correct the table name.
-# table = inst.getTable("datable")
-print("ok")
-
-# model
-model = YOLO('cubeholder.pt')
-
-# object classes
-classNames = ["cubeholder"
-              ]
-
-print("ok2")
+model = YOLO("yolov8n.pt")
+cap1 = cv2.VideoCapture(0)  
+cap1.set(3, 640)
+cap1.set(4, 480)
 
 
 while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
+    _, frame1 = cap1.read()
 
-    print("ok3")
+    img1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
 
-    # coordinates
-    for r in results:
-        print("ok4")
-        boxes = r.boxes
+    results1 = model.predict(img1)
 
-        for box in boxes:
-            print("ok5")
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
 
-            # class name
-            cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
-
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
-
-    cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
-cap.release()
+cap1.release()
 cv2.destroyAllWindows()
